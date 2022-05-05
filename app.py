b747daa1857dba20af0c416e15aea7a038f803b2
Flame <flameyungoxx@gmail.com>
2022-05-05 08:48:05 +0400

    created two tables

--- app.py
+++ app.py
@@ -11,6 +11,31 @@ def db_connection():
         print(error_message)
     return conn
 
+def create_tables():
+    tables = [
+        """CREATE TABLE IF NOT EXISTS artists (
+            ID INTEGER PRIMARY KEY UNIQUE,
+            name TEXT NOT NULL,
+            surname TEXT NOT NULL,
+            age INTEGER NOT NULL,
+            country TEXT NOT NULL
+        )""",
+        """CREATE TABLE IF NOT EXISTS songs (
+            ID INTEGER PRIMARY KEY UNIQUE,
+            album TEXT NOT NULL,
+            song_name TEXT NOT NULL,
+            song_style TEXT NOT NULL,
+            BPM INTEGER NOT NULL,
+            upload_date TEXT NOT NULL,
+            artist_id INTEGER UNIQUE NOT NULL,
+            FOREIGN KEY (artist_id) REFERENCES artists (ID)
+        )"""
+    ]
+    conn = db_connection()
+    cur = conn.cursor()
+    for table in tables:
+        cur.execute(table)
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -18,4 +43,5 @@ def home():
     return "Welcome..."
 
 if __name__ == "__main__":
+    create_tables()
     app.run(port=7777, debug=True)
