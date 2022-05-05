ca18de70ce0d422b73ed5e05c2677fe28047c5f0
Flame <flameyungoxx@gmail.com>
2022-05-05 08:36:23 +0400

    connected sqlite database

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 from flask import Flask
+import sqlite3
+
+DATABASE_NAME = "genius.sqlite"
+
+def db_connection():
+    conn = None
+    try:
+        conn = sqlite3.connect(DATABASE_NAME)
+    except Exception as error_message:
+        print(error_message)
+    return conn
 
 app = Flask(__name__)
 
